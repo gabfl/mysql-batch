9c2a9d59ea89a1a19b249776f9d25f23622d8e4f
gab <gabrielbordeaux@gmail.com>
2017-07-12 11:50:51 -0400

    Add manifest

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ try:
 except(IOError, ImportError):
     long_description = open('README.md').read()
 
-setup (
-    name = 'mysql_batch',
-    version = '1.0.4',
-    description = 'Run large MySQL UPDATE and DELETE queries with small batches to prevent table/row-level locks',
-    long_description = long_description,
-    author = 'Gabriel Bordeaux',
-    author_email = 'pypi@gab.lc',
-    url = 'https://github.com/gabfl/mysql-batch-update',
-    license = 'MIT',
-    packages = ['mysql_batch'],
-    package_dir = { 'mysql_batch': 'src' },
-    install_requires = ['pymysql', 'argparse'], # external dependencies
-    entry_points = {
+setup(
+    name='mysql_batch',
+    version='1.0.5',
+    description='Run large MySQL UPDATE and DELETE queries with small batches to prevent table/row-level locks',
+    long_description=long_description,
+    author='Gabriel Bordeaux',
+    author_email='pypi@gab.lc',
+    url='https://github.com/gabfl/mysql-batch-update',
+    license='MIT',
+    packages=['mysql_batch'],
+    package_dir={'mysql_batch': 'src'},
+    install_requires=['pymysql', 'argparse'],  # external dependencies
+    entry_points={
         'console_scripts': [
             'mysql_batch = mysql_batch.mysql_batch:main',
         ],
