70729326f9eb1ecdc3b8c37bdffb60014385e79e
gab <gabrielbordeaux@gmail.com>
2022-01-29 23:58:19 -0500

    bump PyMySQL 1.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup(
     name='mysql_batch',
-    version='1.2',
+    version='1.2.1',
     description='Run large MySQL UPDATE and DELETE queries with small batches to prevent table/row-level locks',
     long_description=long_description,
     author='Gabriel Bordeaux',
@@ -17,7 +17,9 @@ setup(
     license='MIT',
     packages=['mysql_batch'],
     package_dir={'mysql_batch': 'src'},
-    install_requires=['pymysql', 'argparse'],  # external dependencies
+    install_requires=[  # external dependencies
+        'pymysql==1.0.*'
+    ],
     entry_points={
         'console_scripts': [
             'mysql_batch = mysql_batch.mysql_batch:main',
